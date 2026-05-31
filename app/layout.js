7c567288b0ab6c9901f3f9f986e7083795be4d82
Ahmed Kassem <ahmed.taher.eng@gmail.com>
2026-06-01 00:01:39 +1000

    header iPhone fix-layout update

--- app/layout.js
+++ app/layout.js
@@ -6,16 +6,20 @@ export const metadata = {
     'UX/UI | Product design | Designing products people actually want to use, across every screen and every scale',
 }
 
-export const viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  viewportFit: 'cover',
-}
+// export const viewport = {
+//   width: 'device-width',
+//   initialScale: 1,
+//   viewportFit: 'cover',
+// }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
