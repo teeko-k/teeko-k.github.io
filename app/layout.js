eb3f242bdf0c95db254d688d79fc1dd017ca15bf
Ahmed Kassem <ahmed.taher.eng@gmail.com>
2026-06-01 15:28:31 +1000

    theme color fix added

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="theme-color" content="#e9e9e9" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
