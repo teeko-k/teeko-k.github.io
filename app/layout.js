225f00efa500730102dca2297b8898731fd823ac
Ahmed Kassem <ahmed.taher.eng@gmail.com>
2026-06-06 21:50:38 +1000

    Header restructure 01

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,25 @@
 import { ThemeProvider } from '@/context/ThemeContext'
 import './globals.css'
 
+// title and description are used in the head of the document, and are important for SEO and accessibility. They provide information about the content of the page and help search engines understand what the page is about. The title is typically displayed in the browser tab and search engine results, while the description may be shown in search engine snippets.
 export const metadata = {
   title: 'Ahmed Kassem | UX / Product Designer',
   description:
     'UX/UI | Product design | Designing products people actually want to use, across every screen and every scale',
 }
 
-// export const viewport = {
-//   width: 'device-width',
-//   initialScale: 1,
-//   viewportFit: 'cover',
-// }
+// CRITICAL: The viewport meta tag is essential for responsive web design. It controls how the webpage is displayed on different devices, especially mobile devices. By setting the width to 'device-width', it ensures that the page scales correctly to fit the screen size of the device. The initialScale of 1 means that the page will not be zoomed in or out when first loaded. The viewportFit of 'cover' allows the content to extend into the safe area on devices with a notch or rounded corners, providing a more immersive experience.
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <head>
         <meta name="theme-color" content="#09102a" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
-        />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
--- components/Header.js
+++ components/Header.js
@@ -41,76 +41,71 @@ export default function Header({ isProjectPage = false }) {
 
   return (
     <>
-      {/* Notch fill */}
-      {/* <div
-        className="fixed top-0 left-0 right-0 z-[53] bg-red-500"
-        style={{ minHeight: 'env(safe-area-inset-top)' }}
-      /> */}
-      {/* Sticky Header */}
       {/* <header
-        className={`sticky top-0 left-0 right-0 border-b z-50 flex items-center justify-between px-8 md:px-12 py-6 transition-all duration-500 ${scrolled ? 'bg-color-bg' : 'transparent'}`}
+        className={`fixed top-0 left-0 right-0 border-b z-50 flex items-center justify-between pt-6 pt-[env(safe-area-inset-top)] pb-6 px-8 md:px-12 transition-all duration-500`}
         style={{
-          // paddingTop: '80px !important',
+          // paddingTop: 'calc(env(safe-area-inset-top) + 1.5rem)',
+          // paddingBottom: '1.5rem',
+          // backgroundColor: scrolled ? 'var(--color-bg)' : 'transparent',
+          backgroundColor: 'var(--color-bg)',
           borderColor: scrolled ? 'rgba(25,25,25,0.3)' : 'transparent',
         }}
       > */}
       <header
-        className={`fixed left-0 right-0 border-b z-50 flex items-center justify-between px-8 md:px-12 transition-all duration-500`}
-        style={{
-          top: 'calc(-1 * env(safe-area-inset-top))',
-          paddingTop: 'calc(env(safe-area-inset-top) + 1.5rem)',
-          paddingBottom: '1.5rem',
-          backgroundColor: scrolled ? 'var(--color-bg)' : 'transparent',
-          borderColor: scrolled ? 'rgba(25,25,25,0.3)' : 'transparent',
-        }}
+        className={`fixed top-0 left-0 right-0 z-50 bg-color-bg pt-[env(safe-area-inset-top)]`}
       >
-        {/* Logo */}
-        <a href="/" className="relative z-50 flex flex-col leading-none group">
-          <span
-            className={`${isProjectPage && !scrolled ? 'text-white' : 'text-text-primary'} tracking-widest-3 uppercase text-xs font-sans font-normal`}
-            style={{
-              letterSpacing: '0.3em',
-            }}
+        <div className={`py-6 px-8 md:px-12 flex items-center justify-between`}>
+          {/* Logo */}
+          <a
+            href="/"
+            className="relative z-50 flex flex-col leading-none group"
           >
-            Ahmed 'Tiko' K.
-          </span>
-          <span
-            className={`${isProjectPage && !scrolled ? 'text-white' : 'text-text-secondary'} text-sm font-[200] font-sans mt-0.5`}
-            style={{ letterSpacing: '0.2em', fontSize: '12px' }}
-          >
-            UX | Product Designer
-          </span>
-        </a>
+            <span
+              className={`${isProjectPage && !scrolled ? 'text-white' : 'text-text-primary'} tracking-widest-3 uppercase text-xs font-sans font-normal`}
+              style={{
+                letterSpacing: '0.3em',
+              }}
+            >
+              Ahmed 'Tiko' K.
+            </span>
+            <span
+              className={`${isProjectPage && !scrolled ? 'text-white' : 'text-text-secondary'} text-sm font-[200] font-sans mt-0.5`}
+              style={{ letterSpacing: '0.2em', fontSize: '12px' }}
+            >
+              UX | Product Designer
+            </span>
+          </a>
 
-        {/* Burger Button */}
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="relative z-50 flex flex-col justify-center items-end gap-1.5 w-10 h-10 group"
-          aria-label="Toggle navigation"
-        >
-          <span
-            className={`burger-line block h-[2px] ${isProjectPage && !scrolled && !isOpen ? 'bg-white' : 'bg-text-secondary'} `}
-            style={{
-              width: '28px',
-              transform: isOpen ? 'translateY(7px) rotate(45deg)' : 'none',
-            }}
-          />
-          <span
-            className={`burger-line block h-[2px] ${isProjectPage && !scrolled && !isOpen ? 'bg-white' : 'bg-text-secondary'} `}
-            style={{
-              width: '20px',
-              opacity: isOpen ? 0 : 1,
-              transform: isOpen ? 'scaleX(0)' : 'none',
-            }}
-          />
-          <span
-            className={`burger-line block h-[2px] ${isProjectPage && !scrolled && !isOpen ? 'bg-white' : 'bg-text-secondary'} `}
-            style={{
-              width: '25px',
-              transform: isOpen ? 'translateY(-7px) rotate(-45deg)' : 'none',
-            }}
-          />
-        </button>
+          {/* Burger Button */}
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="relative z-50 flex flex-col justify-center items-end gap-1.5 w-10 h-10 group"
+            aria-label="Toggle navigation"
+          >
+            <span
+              className={`burger-line block h-[2px] ${isProjectPage && !scrolled && !isOpen ? 'bg-white' : 'bg-text-secondary'} `}
+              style={{
+                width: '28px',
+                transform: isOpen ? 'translateY(7px) rotate(45deg)' : 'none',
+              }}
+            />
+            <span
+              className={`burger-line block h-[2px] ${isProjectPage && !scrolled && !isOpen ? 'bg-white' : 'bg-text-secondary'} `}
+              style={{
+                width: '20px',
+                opacity: isOpen ? 0 : 1,
+                transform: isOpen ? 'scaleX(0)' : 'none',
+              }}
+            />
+            <span
+              className={`burger-line block h-[2px] ${isProjectPage && !scrolled && !isOpen ? 'bg-white' : 'bg-text-secondary'} `}
+              style={{
+                width: '25px',
+                transform: isOpen ? 'translateY(-7px) rotate(-45deg)' : 'none',
+              }}
+            />
+          </button>
+        </div>
       </header>
 
       <OverlayNav
