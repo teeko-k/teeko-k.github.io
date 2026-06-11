b0358c8d5fbe443929ccc0341b2c3f49f2af6efd
Ahmed Kassem <ahmed.taher.eng@gmail.com>
2026-06-11 11:53:30 +1000

    Contact Fix

--- components/Contact.js
+++ components/Contact.js
@@ -49,7 +49,7 @@ export default function Contact() {
     <section
       id="contact"
       ref={sectionRef}
-      className="pb-24 pt-0 px-8 md:px-12 relative"
+      className="pb-24 pt-0 px-8 md:px-12 xl:max-w-[1280px] xl:mx-auto relative"
     >
       {/* Bordered box */}
       <div
