1dbc19eb2d67f7c79f69c3bda41c7b0fe3e86e29
Ahmed Kassem <ahmed.taher.eng@gmail.com>
2026-06-11 15:07:38 +1000

    Hero big screen fix

--- components/Hero.js
+++ components/Hero.js
@@ -8,7 +8,7 @@ export default function Hero() {
 
   return (
     <section
-      className="bg-color-bg relative h-screen 2xl:h-[80vh] flex flex-col justify-end overflow-hidden"
+      className="bg-color-bg relative h-screen 2xl:h-[80vh] flex flex-col justify-end 2xl:justify-center 2xl:pt-[200px] overflow-hidden"
       id="home"
     >
       {/* Background Gradient */}
